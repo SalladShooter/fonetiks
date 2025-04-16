33c23502a1349cc906c429cd083363b7051d8955
Gorganism <gogium@proton.me>
2025-04-15 22:03:59 -0500

    Add files via upload

---
+++ fonetiks.py
@@ -0,0 +1,36 @@
+english = input('english to convert:\n')
+thornmode = input(':: is thorn mode enabled? [Y/n] ')
+while thornmode != 'Y' and thornmode != 'n':
+    thornmode = input(':: that didn\'t work. is thorn mode enabled? [Y/n] ')
+if thornmode == 'Y':
+    english = english.replace('th','þ')
+elif thornmode == 'n':
+    english = english.replace('th','ð')
+else:
+    print('sorry, thornmode toggle could not be determined.') #might be redundant
+english = english.replace('sh','ʃ')
+english = english.replace('tio','ʃo')
+english = english.replace('co','ko') #soft c -> s
+english = english.replace('cu','ku') #hard c -> k
+english = english.replace('ca','ka') #ch sound -> c
+english = english.replace('ic','ik')
+english = english.replace('ci','si')
+english = english.replace('ce','se')
+english = english.replace('ch','c')
+english = english.replace('cem','kem')
+english = english.replace('ng','ŋ')
+english = english.replace('ph','f')
+english = english.replace('cough','koff')
+english = english.replace('laugh','laff')
+english = english.replace('gh','')
+english = english.replace('ax','aks')
+english = english.replace('ox','oks')
+english = english.replace('ux','uks')
+english = english.replace('ix','iks')
+english = english.replace('ex','eks')
+english = english.replace('x','z')
+english = english.replace('oo','u')
+# english = english.replace('ge','j')
+english = english.replace('throu','thru')
+print('sorry this may not be perfect (it isnt) and you may need to make edits after.\nbut the output is:\n'+english)
+
---
+++ readme.txt
@@ -0,0 +1,3 @@
+hello feel free to suggest things, the main goal of this project is to make a more phonetic syntax for english whilst preserving pronunciation i guess thank you
+
+hello feel free to suggest þiŋs, þe main goal of þis project is to make a more fonetik syntaks for eŋliʃ whilst preserviŋ pronunsiaʃon i guess þank you
